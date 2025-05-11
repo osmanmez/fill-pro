d2b0aadc77c3707f00e0eb3d186f12d8d392afa9
Osman Otoniel Mazariegos Mendez <omazariegosmz@gmail.com>
2025-05-10 20:51:50 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
